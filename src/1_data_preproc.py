077f634abf2d95d701f67d0e1af42f6e943af5eb
Thiago Fleck <tfleck@gmail.com>
2018-11-10 12:38:42 -0200

    Rodada 33

--- src/1_data_preproc.py
+++ src/1_data_preproc.py
@@ -8,7 +8,7 @@ Created on 2018-09-13
 import numpy as np
 import pandas as pd
 
-CURRENT_ROUND = 31
+CURRENT_ROUND = 33
 
 # Load data from all 2018 rounds
 # Data from https://github.com/henriquepgomide/caRtola
@@ -43,6 +43,8 @@ rounds.append(pd.read_csv('../data/rodada-27.csv'))
 rounds.append(pd.read_csv('../data/rodada-28.csv'))
 rounds.append(pd.read_csv('../data/rodada-29.csv'))
 rounds.append(pd.read_csv('../data/rodada-30.csv'))
+rounds.append(pd.read_csv('../data/rodada-31.csv'))
+rounds.append(pd.read_csv('../data/rodada-32.csv'))
 
 df = pd.concat(rounds)
 #df.shape[0]
